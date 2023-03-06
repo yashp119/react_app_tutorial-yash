058434147ea957fb22a92a32671574c098e53e21
Aliraza Khan <itsarkcodes@gmail.com>
2023-03-06 20:40:34 +0530

    Changing todo list

--- src/services/todo.js
+++ src/services/todo.js
@@ -8,7 +8,7 @@ export function getAll() {
     return [
         {
             id: 1,
-            text: 'Learn Javascript',
+            text: 'New Video',
             completed: false
         },
         {
